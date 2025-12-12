c870d21fa60c65ca0fce746830b09e0ad729ec3a
Wendell Santos Andrade <wendellsantoslyn@gmail.com>
2025-12-11 22:03:53 -0300

    03/12/2025

---
+++ lib/dados.dart
@@ -0,0 +1,10 @@
+import 'pergunta_respostas.dart';
+
+final List<PerguntaRespostas> perguntasRespostas = [
+  PerguntaRespostas(pergunta : "Qual a sua cor favorita?",
+    respostas : ["verde", "amarelo", "azul", "vermelho", "roxo"]),
+  PerguntaRespostas(pergunta : "Qual o seu animal favorito?",
+    respostas : ["Garfield", "lasanha do Garfield", "humano do Garfield", "cachorro amigo do Garfield", "galo"]),
+  PerguntaRespostas(pergunta : "Qual é o seu time?",
+    respostas : ["Josefense", "Flamengo do Piauí", "Capivariano", "Corinthians Alagoano", "SER Panambi"]),
+];
